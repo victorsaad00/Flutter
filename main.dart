87562dcff78f49b283033786095cfa9d71479382
Victor Cavalcanti Saad <22480813+victorsaad00@users.noreply.github.com>
2020-08-25 11:45:23 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,180 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DSIApp());
+}
+
+class DSIApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DSI App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DSIPage(title: 'My First App - DSI/BSI/UFRPE'),
+    );
+  }
+}
+
+class DSIPage extends StatefulWidget {
+  final String title;
+
+  DSIPage({Key key, this.title}) : super(key: key);
+
+  @override
+  _DSIPageState createState() => _DSIPageState();
+}
+
+class _DSIPageState extends State<DSIPage> {
+  final _warnings = [
+    'JÁ DEU BENÇA!',
+    'VAI QUEBRAR O CELULAR!',
+    'PARA VÉI!',
+    'QUE DEDO NERVOSO!',
+    'PRA QUE ISSO?!',
+  ];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _generateHyperClick(){
+    Random rand = new Random();
+    int selector = rand.nextInt(50);
+    if (selector == 1 || selector == 50 ){
+      setState(() {
+        _counter += rand.nextInt(100) + 60;
+      });
+    } else if (selector == 10) {
+      setState(() {
+        _counter -= rand.nextInt(100) + 20;
+      });
+    } else {
+      setState(() {
+        _counter++;
+      });
+    }
+
+  }
+
+  String _getCountText() {
+    return 'Você clicou $_counter vezes no botão.';
+  }
+
+  String _getWarningText() {
+    String result = '';
+    if (_counter > 5) {
+      var idx = Random().nextInt(_warnings.length);
+      result = _warnings[idx];
+    }
+    return result;
+  }
+
+  String _getImageName() {
+    //abra o arquivo 'pubspec.yaml' e veja as entradas na seção 'assets'.
+    //para incluir novas imagens, basta incluir novas entradas nesta seção.
+    String result;
+    if (_counter == 0) {
+      result = '';
+    } else if (_counter > 5) {
+      result = 'images/jadeu.png';
+    } else if (_counter > 2) {
+      result = 'images/thug2.gif';
+    } else {
+      result = 'images/thug1.jpg';
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Text(widget.title),
+            Spacer(),
+            Image(image: AssetImage('images/logo/bsi-white.png'), height: 32),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              DSIMainBodyWidget(
+                  _getCountText(), _getWarningText(), _getImageName()),
+              Spacer(),
+              FlatButton(
+                onPressed: _resetCounter,
+                color: Colors.blueAccent,
+                child: Text(
+                  'Reset',
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                )
+              ),
+              FlatButton(
+                onPressed: _generateHyperClick,
+                color: Colors.redAccent,
+                child: Text(
+                  'Aposte sua sorte!',
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+              )],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DSIMainBodyWidget extends StatelessWidget {
+  final String _countText;
+  final String _warningText;
+  final String _image;
+  DSIMainBodyWidget(this._countText, this._warningText, this._image);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          _countText,
+          style: TextStyle(color: Colors.black, fontSize: 16.0),
+        ),
+        SizedBox(height: 8.0),
+        if (_warningText.isNotEmpty)
+          Text(
+            _warningText,
+            style: TextStyle(color: Colors.red, fontSize: 24.0),
+          ),
+        SizedBox(height: 8.0),
+        if (_image.isNotEmpty)
+          Image(
+            image: AssetImage(_image),
+            height: 240,
+          ),
+      ],
+    );
+  }
+}
